b1b7fd0125cebeba0f1dadba747f5c8ba7b47456
Armedi <armedi@outlook.com>
2020-01-22 01:44:16 +0700

    - bumped @google-cloud/storage version - keyFilename now can be set with GOOGLE_APPLICATION_CREDENTIALS environment variable - better way of handling asynchronousity in .fields method - added option for custom prefix

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ It will change the field for the uploaded file with a url string like http://sto
 ### Requirements
 1. Make sure you have a google cloud project with billing enabled.
 2. [Enable Google Storage API](https://console.cloud.google.com/flows/enableapi?apiid=storage-api.googleapis.com) for the project.
-3. [Create a service account](https://console.cloud.google.com/apis/credentials/serviceaccountkey) and download the credential in JSON format.
+3. [Create a bucket](https://console.cloud.google.com/storage/create-bucket) to store the files.
+4. [Create a service account](https://console.cloud.google.com/apis/credentials/serviceaccountkey) and download the credential in JSON format.
 
 
 ## Installation
@@ -25,8 +26,12 @@ npm install gcs-upload
 - _gcsConfig_:
   - keyFilename: file path for credential that you have downloaded before.
   - bucketName: the bucket name that will contain the uploaded file, you can create one through google cloud console.
+  - prefix (optional): it's a string or a function that return a string which will be used as a prefix for filenames being stored. 
 
 It will return an upload object that have 3 methods: (.single(), .array(), and .fields()). You can use all of them just like how you would use [multer](https://github.com/expressjs/multer#singlefieldname).
+
+note:
+> Instead of providing keyFilename in config, you can also use `GOOGLE_APPLICATION_CREDENTIALS` environment variable to store absolute path to the credential file.
 
 
 ### Basic Example
@@ -51,8 +56,9 @@ const upload = gcsUpload({
     fileSize: 1e6 // in bytes
   },
   gcsConfig: {
-    keyFilename: '/Users/me/google-credential-keyfile.json',
-    bucketName: 'my-bucket'
+    keyFilename: '/Users/me/google-credential-keyfile.json',  // this can also be set using GOOGLE_APPLICATION_CREDENTIALS environment variable 
+    bucketName: 'my-bucket',
+    prefix: () => `${Date.now()}-` // optional, this is it's default value
   }
 })
 
--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 const multer = require('multer')
-const uploadToGcs = require('./uploadToGcs')
+const createBucket = require('./createBucket')
 
 module.exports = ({ limits, gcsConfig }) => {
   const multerStorage = multer.memoryStorage()
   const upload = multer({ storage: multerStorage, limits })
 
+  const saveToBucket = createBucket(gcsConfig)
+
   return {
-    single: (fieldname) => [
+    single: fieldname => [
       upload.single(fieldname),
       async (req, res, next) => {
         if (!req.file) return next()
         try {
-          req.body[fieldname] = await uploadToGcs({ file: req.file, gcsConfig })
+          req.body[fieldname] = await saveToBucket(req.file)
           next()
         } catch (error) {
           next(error)
         }
-      }
+      },
     ],
 
     array: (fieldname, maxCount) => [
       upload.array(fieldname, maxCount),
       async (req, res, next) => {
         try {
-          req.body[fieldname] = await Promise.all(req.files.map(file => uploadToGcs({ file, gcsConfig })))
+          req.body[fieldname] = await Promise.all(req.files.map(saveToBucket))
           next()
         } catch (error) {
           next(error)
         }
-      }
+      },
     ],
 
-    fields: (fields) => [
+    fields: fields => [
       upload.fields(fields),
       async (req, res, next) => {
         try {
-          const links = await Promise.all(Object.entries(req.files)
-            .map(async ([fieldname, files]) => ({
-              [fieldname]: await Promise.all(files.map(file =>
-                uploadToGcs({ file, gcsConfig })
-              ))
-            }))
-          )
+          const links = (await Promise.all(
+            Object.entries(req.files)
+              .map(([fieldname, files]) => Promise.all([fieldname, Promise.all(files.map(saveToBucket))]))
+          ))
+            .map(([fieldname, files]) => ({ [fieldname]: files }))
+
           Object.assign(req.body, ...links)
           next()
         } catch (error) {
           next(error)
         }
-      }
-    ]
+      },
+    ],
   }
 }
